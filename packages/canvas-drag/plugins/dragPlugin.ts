42ea3022df186b2f8a71bc6cd42cd08c012fa420
陈羽云 <897908015@qq.com>
2025-12-11 01:08:38 +0800

    feat：添加拖拽插件

---
+++ packages/canvas-drag/plugins/dragPlugin.ts
@@ -0,0 +1,18 @@
+import { Plugin, RectInfo } from "../types";
+
+export const dragPlugin: Plugin = {
+  name: "dragPlugin",
+  move({ mouse, dispatch, selected, selectIds, activeTool }) {
+    if (activeTool !== "drag") return;
+
+    const { disY, disX } = mouse;
+    const data: Record<string, Partial<RectInfo>> = {};
+    for (let i = 0; i < selectIds.length; i++) {
+      const id = selectIds[i];
+      const el = selected[id];
+      data[id] = { left: parseInt(`${el.left}`) + disX, top: parseInt(`${el.top}`) + disY };
+    }
+    console.log(222);
+    dispatch("UPDATE_ELEMENT", data);
+  },
+};
