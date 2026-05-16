a2bf26bc0bded2075121c84d669a734278869232
ChenYuYun <897908015@qq.com>
2026-05-16 17:00:44 +0800

    add：测试配置组件

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { reactive } from "vue";
-import { ConfigTypes, RenderItem } from "@agile-ui/ui";
+import { ConfigTypes, NodeTypes, RenderItem, RenderNode } from "@agile-ui/ui";
 import {
   CanvasDrag,
   collisionPlugin,
@@ -14,7 +14,8 @@ import {
   type ElementType,
   type Plugin,
 } from "@agile-ui/canvas-drag";
-import { createExpressionEngine, Types } from "@agile-ui/expr-engine";
+import { createExpressionEngine, CustomFunctionDef, CustomFunctionManager, Sandbox } from "@agile-ui/expr-engine";
+import registry from "./components";
 
 defineOptions({ name: "App" });
 // const a = ref("222");
@@ -79,21 +80,124 @@ const plugins: Plugin[] = [
 // console.log(parser.parse("(11 + 12) * 16 + 12"));
 // console.log(parser.parse("!(value && data.a)"));
 const run = async () => {
-  const engine = createExpressionEngine();
-  const ctx = engine.createContext({ a: 11, b: 2 });
-  engine.registry.register({
-    name: "add",
-    signature: { paramTypes: [Types.number, Types.number], returnType: Types.number },
-    execute: (args) => {
-      const a = args[0] as number;
-      const b = args[1] as number;
-      return a + b;
-    },
+  const engine = createExpressionEngine({
+    timeout: 5000,
   });
-  console.log(await engine.compiler.execute("2222", ctx));
-  console.log(await engine.compiler.execute("add(a, b) * 3", ctx));
+  // const ctx = engine.createContext({
+  //   a: 11,
+  //   b: 10,
+  //   users: [
+  //     { name: "Alice", status: "active", num: 3 },
+  //     { name: "Bob", status: "inactive", num: 13 },
+  //     { name: "Charlie", status: "active", num: 10 },
+  //   ],
+  // });
+  // engine.registry.register({
+  //   name: "add",
+  //   signature: { paramTypes: [Types.number, Types.number], returnType: Types.number },
+  //   execute: (args) => {
+  //     const a = args[0] as number;
+  //     const b = args[1] as number;
+  //     return a + b;
+  //   },
+  // });
+  // engine.registry.register({
+  //   name: "isActive",
+  //   signature: { paramTypes: [Types.any], returnType: Types.boolean },
+  //   execute: (args) => {
+  //     const user = args[0] as { name: string; status: string };
+  //     return user.status === "active";
+  //   },
+  // });
+  // console.log(await engine.compiler.execute("2222", ctx));
+  // console.log(await engine.compiler.execute("add(a, b) * 3", ctx));
+  // console.log(await engine.compiler.execute("(1 + 3) * 3", ctx));
+  // console.log(await engine.compiler.execute("users[0].num", ctx));
+  // console.log(await engine.compiler.execute("arrFilter(users, 'isActive')", ctx));
+  // console.log(await engine.compiler.execute("arrFilter(users, (item, i) => item.num > b)", ctx));
+
+  const pageRegistry = engine.createPageRegistry();
+  const sandbox = new Sandbox(pageRegistry, { timeout: 5000 });
+  const funcManager = new CustomFunctionManager(pageRegistry, sandbox);
+
+  // 定义两个自定义函数
+  const customFuncs: CustomFunctionDef[] = [
+    {
+      id: "fn1",
+      name: "fullName",
+      description: "拼接名字",
+      params: [{ name: "user" }],
+      body: `user.firstName + ' ' + user.lastName`,
+      returnType: "string",
+    },
+    {
+      id: "fn2",
+      name: "adult",
+      description: "查找成年",
+      params: [{ name: "person" }],
+      body: `person.age >= age`,
+      returnType: "boolean",
+    },
+  ];
+  funcManager.registerAll(customFuncs);
+
+  const pageContext = engine.createContext(
+    {
+      age: 18,
+      name: "chen",
+      users: [
+        { firstName: "Alice", lastName: "Wang", age: 25 },
+        { firstName: "Bob", lastName: "Li", age: 17 },
+      ],
+      keys: { key: "age" },
+      userInfo: { name: "yu", age: 22 },
+    },
+    pageRegistry,
+  );
+
+  console.log(await engine.compiler.execute(`srtConcat(toString(age), name)`, pageContext));
+  console.log(await engine.compiler.execute(`11 * (1 + 3) - age / 3`, pageContext));
+  // 使用 Lambda + 自定义函数
+  const names = await engine.compiler.execute(`arrMap(users, (u) => fullName(u))`, pageContext);
+  console.log(names); // [ 'Alice Wang', 'Bob Li' ]
+
+  const adults = await engine.compiler.execute(`arrFilter(users, (u) => adult(u))`, pageContext);
+  console.log(adults); // [ { firstName: 'Alice', ... age: 25 } ]
+
+  const age = await engine.compiler.execute("userInfo[keys.key]", pageContext);
+  console.log("age", age);
 };
 run();
+
+/**
+ * 事件链
+ * 1.事件模型（统一事件结构，支持事件上下文，事件传播机制（冒泡 or 精准触发），事件命名空间）
+ * 2.事件链编排（顺序执行，条件分支，并行执行，循环 / 重试，子流程 / 复用）
+ * 3.执行引擎（执行模式，状态管理，错误处理）
+ * 4.数据流系统（节点输入/输出映射，表达式系统，数据作用域）
+ * 5.节点系统
+ * 6.调试与可观测性（可视化调试，日志系统，执行回放，性能监控）
+ * 7.权限与安全
+ */
+
+const nodeData = reactive({ name: "陈", age: 23, sex: "男" });
+
+const node = reactive<NodeTypes>({
+  id: "root",
+  component: "YDetail",
+  props: {
+    items: [
+      { label: "名字：", key: "name" },
+      { label: "年龄：", key: "age" },
+      { label: "性别：", key: "sex" },
+    ],
+    data: nodeData,
+  },
+});
+const context = reactive({});
+setTimeout(() => {
+  nodeData.name = "3333";
+}, 2000);
 </script>
 
 <template>
@@ -102,4 +206,6 @@ run();
       <RenderItem :data="data" :item="item" />
     </template>
   </CanvasDrag>
+
+  <RenderNode :node="node" :context="context" :custom-components="registry" />
 </template>
